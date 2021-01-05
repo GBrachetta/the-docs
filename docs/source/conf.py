87a9f7e1c97cd8ba49159abd390bb8a6e074bfab
Guillermo Brachetta <brachetta@me.com>
2021-01-05 19:27:28 +0100

    Versioned change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, G.Brachetta"
 author = "G.Brachetta"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
